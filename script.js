d6d7eec4f46c68031fbce0e4bef18feee00830c1
Javad <1998javad@gmail.com>
2022-12-09 10:32:17 +0100

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGC_tIgA0YBIWtTUi9gbl6iV0HyVsffZE",
+  authDomain: "movie-51741.firebaseapp.com",
+  projectId: "movie-51741",
+  storageBucket: "movie-51741.appspot.com",
+  messagingSenderId: "821539652040",
+  appId: "1:821539652040:web:2e9be0cdd1ba29228fc0fd"
+};
+
+const app = initializeApp(firebaseConfig);
